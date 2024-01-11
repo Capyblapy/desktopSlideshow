e12500bf35f1035d2be65f6322aa526316854125
Capyblapy <111537871+Capyblapy@users.noreply.github.com>
2024-01-10 22:01:40 -0600

    Add files via upload

---
+++ paths.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+
+try:
+    if not os.path.exists(os.path.join(Path.home(),"Desktop", "desktopSlideshow", "transitionFrames")):
+        os.makedirs(os.path.join(Path.home(),"Desktop", "desktopSlideshow", "transitionFrames"))
+except OSError:
+    print ('Error: Creating directory of transitionFrames')
+
+path = os.path.join(Path.home(),"Desktop", "desktopSlideshow", "deer")
+transitionPath = os.path.join(Path.home(),"Desktop", "desktopSlideshow", "transitionFrames")
+videoPath = os.path.join(Path.home(),"Desktop", "desktopSlideshow", "transition.mp4")
